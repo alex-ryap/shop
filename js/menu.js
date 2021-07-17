e2312b54a1d8bc3fc5b378a6b7e2d70588b1c8ff
alexryapolov <alexryapolov36@gmail.com>
2021-07-17 15:13:30 +0400

    add cart

--- js/menu.js
+++ js/menu.js
@@ -1,8 +1,7 @@
+// MENU
 const btnMenu = document.querySelector('.navigation__btn');
 const btnMenuClose = document.querySelector('.menu__btn_close');
 const menu = document.querySelector('.menu');
-
-console.log(menu);
 
 btnMenu.addEventListener('click', () => {
   menu.classList.toggle('menu__open');
